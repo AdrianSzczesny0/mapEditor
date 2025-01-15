4a42b4822bb7fb4d538fa4e5495494e85a6fe8a2
aszczesny <aszczesny@beeline.com>
2025-01-15 17:54:36 +0100

    test read from file on serverside

--- server/server.js
+++ server/server.js
@@ -3,6 +3,33 @@ const express = require ('express');
 const app = express();
 const PORT = 8085;
 
+// this is working
+// const fs = require('node:fs');
+// const config = require('./test.json');
+// console.log(config);
+
+
+const fs = require("fs");
+
+const books =
+{
+    title: "Alli goes to school",
+    genre: "Fiction",
+    type: "Children",
+    pages: 56
+};
+
+const jsonData = JSON.stringify(books, null, 2);
+
+fs.writeFile("./server/test.json", jsonData, 'utf8', (err) => {
+    if (err) {
+        console.error('Error writing to file', err);
+    } else {
+        console.log('Data written to file');
+    }
+});
+
+
 app.get('/test', (req, res) =>{
     res.status(200).send({
         message:'testing'
